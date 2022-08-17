f233afd1dd58743312e96975aa393f7b9cba702f
danielontivero <danielontivero@hotmail.com>
2022-08-17 10:57:36 -0300

    java mi proyecto v.1.1

---
+++ src/components/NavBar.js
@@ -0,0 +1,19 @@
+
+const NavBar = () => {
+    return (
+<nav className ="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a className ="navbar-brand" href="/" >Home</a>
+  <button className ="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span className ="navbar-toggler-icon"></span>
+  </button>
+  <div className ="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div className ="navbar-nav">
+      <a className ="nav-item nav-link active" href="/">Productos</a>
+      <a className ="nav-item nav-link" href="/">Contacto</a>
+      <a className ="nav-item nav-link" href="/">Sobre Nosotros</a>
+    </div>
+  </div>
+</nav>
+    );
+};  
+export default NavBar;
