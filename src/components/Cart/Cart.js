8d55a5849c1d73dd74001cdddf93ad2c707a8789
danielontivero <danielontivero@hotmail.com>
2022-09-30 14:30:06 -0300

    entrega 30-9

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,11 +2,32 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import { useCartContext } from '../CartContext/CarContext';
 import ItemCart from '../ItemCart/ItemCart';
+import { addDoc, collection, getFirestore } from 'firebase/firestore';
 
 
 export default function Cart() {
   
   const {cart, totalPrice} = useCartContext();
+
+  const order = {
+    buyer: {
+        name: 'Exequiel',
+        email: 'exequiel@gmail.com',
+        phone: '123645988',
+        address:'Rio Blanco 1000'
+    },
+    items: cart.map(product => ({ id: product.id, title: product.nombre, precio: product.precio, quantity: product.quantity})), 
+    total: totalPrice(), 
+  }
+
+
+  const handleClick = () =>{
+    const db = getFirestore();
+    const ordersCollection = collection(db, "orders");
+    addDoc(ordersCollection, order)
+    .then(({ id }) => console.log(id))
+
+  }
 
   if(cart.length === 0) {
     return (
@@ -25,6 +46,7 @@ export default function Cart() {
     }  
     </div>
     <h1> total: $ {totalPrice()} </h1>
+    <button onClick={handleClick}>Emitir Compra</button>
     </>
   )
 }
